193d33f5de9075f8fab26d43d5048fef52cb85bd
Rasmus Beck <rasmusbeck2k@outlook.com>
2022-12-02 16:55:33 +0100

    day2 done

--- day2/app/Main.hs
+++ day2/app/Main.hs
@@ -3,8 +3,8 @@ module Main where
 import Prelude hiding (lookup)
 import RockPaperScissors
 import System.IO
-import Data.Map hiding (map, mapMaybe)
-import Data.Maybe (mapMaybe)
+import qualified Data.Map as Map
+import Data.Maybe (mapMaybe, fromJust)
 
 theirPlay :: String -> RPS
 theirPlay "A" = Rock
@@ -18,12 +18,33 @@ myPlay "Y" = Paper
 myPlay "Z" = Scissors
 myPlay _   = error "invalid input"
 
+-- represents what outcome we want the opponent to have
+theirOutcome :: String -> Outcome
+theirOutcome "X" = Win
+theirOutcome "Y" = Draw
+theirOutcome "Z" = Lose
+theirOutcome _   = error "invalid input"
+
+-- turns a string like "A Y" into a tuple representing (my choice, their choice)
 getPlay :: String -> (RPS, RPS)
 getPlay s = (myPlay $ sWords !! 1, theirPlay $ sWords !! 0)
   where sWords = words s
 
 getWinScore :: RPS -> RPS -> Maybe Integer
-getWinScore myMove theirMove = lookup theirMove (winScoreMap myMove)
+getWinScore myMove theirMove = Map.lookup theirMove (winScoreMap myMove)
+
+invMap :: (Ord k, Ord v) => Map.Map k v -> Map.Map v k
+invMap = Map.foldrWithKey (flip Map.insert) Map.empty
+
+getPlay2 :: String -> (RPS, RPS)
+getPlay2 s = (mine, theirs)
+  where
+    sWords = words s 
+    theirs = theirPlay $ sWords !! 0
+    mine = fromJust $ Map.lookup (theirOutcome $ sWords !! 1) (invMap $ outcomeMap theirs)
+    -- since we use the opponent's move to determine which outcomeMap to use,
+    -- we must also use the desired outcome *for the opponent* as the key
+
 
 main :: IO ()
 main = do
@@ -32,5 +53,12 @@ main = do
   let plays = map getPlay fileLines
   let winScores = mapMaybe (uncurry getWinScore) plays
   let choiceScores = map (choiceScore . fst) plays
+
   print $ sum winScores + sum choiceScores
+
+  let plays2 = map getPlay2 fileLines
+  let winScores2 = mapMaybe (uncurry getWinScore) plays2
+  let choiceScores2 = map (choiceScore . fst) plays2
+
+  print $ sum winScores2 + sum choiceScores2
   
--- day2/src/RockPaperScissors.hs
+++ day2/src/RockPaperScissors.hs
@@ -2,7 +2,8 @@ module RockPaperScissors where
 
 import Data.Map
 
-data RPS = Rock | Paper | Scissors deriving (Ord, Eq)
+data RPS = Rock | Paper | Scissors deriving (Ord, Eq, Show)
+data Outcome = Lose | Draw | Win deriving (Ord, Eq, Show)
 
 winScoreMap :: RPS -> Map RPS Integer
 winScoreMap Rock     = fromList [(Rock, 3), (Paper, 0), (Scissors, 6)]
@@ -13,3 +14,8 @@ choiceScore :: RPS -> Integer
 choiceScore Rock = 1
 choiceScore Paper = 2
 choiceScore Scissors = 3
+
+outcomeMap :: RPS -> Map RPS Outcome
+outcomeMap Rock     = fromList [(Rock, Draw), (Paper, Lose), (Scissors, Win)]
+outcomeMap Paper    = fromList [(Rock, Win), (Paper, Draw), (Scissors, Lose)]
+outcomeMap Scissors = fromList [(Rock, Lose), (Paper, Win), (Scissors, Draw)]
